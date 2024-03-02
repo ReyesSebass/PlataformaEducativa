185f6e30f8977a6e64073a38feb3f93084785d67
Victor Li <Victorlifeng236@gmail.com>
2024-03-01 23:10:00 -0600

    Commit Victor

---
+++ src/test/java/PlataformaEdu/PlataformaEducativaApplicationTests.java
@@ -0,0 +1,13 @@
+package PlataformaEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaEducativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

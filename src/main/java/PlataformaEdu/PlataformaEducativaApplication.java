b5a36f67720909183578fce4d97f136e9cb18c34
Sebastian_yrb <123045887+ReyesSebass@users.noreply.github.com>
2024-03-16 15:51:53 -0600

    Modificacion prueba Signed-off-by:Sebastian_yrb <123045887+ReyesSebass@users.noreply.github.com>

--- src/main/java/PlataformaEdu/PlataformaEducativaApplication.java
+++ src/main/java/PlataformaEdu/PlataformaEducativaApplication.java
@@ -3,6 +3,7 @@ package PlataformaEdu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Main
 @SpringBootApplication
 public class PlataformaEducativaApplication {
 

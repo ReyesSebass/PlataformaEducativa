126f6c961d4f8573cfdd5b0879773b2fe61b052e
Sebastian_yrb <123045887+ReyesSebass@users.noreply.github.com>
2024-04-18 10:38:59 -0600

    Primer Commit Segundo Repositorio. Se arreglo el proyecto, queda funcional se agregaron la parte de los usuarios y roles, tambien implmentando apenas la seguridad
    Signed-off-by:Sebastian_yrb <123045887+ReyesSebass@users.noreply.github.com>

---
+++ src/main/java/PlataformaEdu/dao/RolDAO.java
@@ -0,0 +1,8 @@
+package PlataformaEdu.dao;
+
+import PlataformaEdu.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDAO extends JpaRepository<Rol, Long> {
+
+}
